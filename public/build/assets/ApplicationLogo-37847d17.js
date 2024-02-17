ac0f1aaaf561f534926df3cbe53a971db89b371b
edupoch <edupoch@gmail.com>
2024-02-17 19:51:17 +0100

    Incluimos os assets compilados por Vite no repositorio

---
+++ public/build/assets/ApplicationLogo-37847d17.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{o as e,f as s}from"./app-bd7b2aed.js";const t="/build/assets/logo_peq-d4e75ae7.png",c={},r={src:t};function n(_,a){return e(),s("img",r)}const m=o(c,[["render",n]]);export{m as A};
