b3ca32e071dc77605b7ea367f0bcad4857ade10d
edupoch <edupoch@gmail.com>
2024-02-17 18:41:06 +0100

    Correximos o nome da carpeta da app

--- server_ssl_renew.sh
+++ server_ssl_renew.sh
@@ -2,11 +2,11 @@
 
 # Para facer que este script se execute automáticamente, hai que engadilo a crontab
 # sudo crontab -e
-# 0 12 * * * /var/www/app.revolteira.gal/server_ssl_renew.sh >> /var/log/cron.log 2>&1
+# 0 12 * * * /var/www/app/server_ssl_renew.sh >> /var/log/cron.log 2>&1
 
 COMPOSE="/usr/bin/docker compose --ansi never"
 DOCKER="/usr/bin/docker"
 
-cd /var/www/app.revolteira.gal
+cd /var/www/app
 $COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP nginx
 $DOCKER system prune -af
